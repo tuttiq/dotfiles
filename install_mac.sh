11c1f1d97cefbf1eb4b6637620391bbb1f56a1d3
Tutti Quintella <tuane.quintella@quipper.com>
2018-07-22 22:39:39 +0900

    minor adjustments

--- install_mac.sh
+++ install_mac.sh
@@ -492,8 +492,8 @@ bot "Dock & Dashboard"
 running "Enable highlight hover effect for the grid view of a stack (Dock)"
 defaults write com.apple.dock mouse-over-hilite-stack -bool true;ok
 
-running "Set the icon size of Dock items to 30 pixels"
-defaults write com.apple.dock tilesize -int 30;ok
+running "Set the icon size of Dock items to 36 pixels"
+defaults write com.apple.dock tilesize -int 36;ok
 
 running "Change minimize/maximize window effect to scale"
 defaults write com.apple.dock mineffect -string "scale";ok
